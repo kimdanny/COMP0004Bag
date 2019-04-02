357088260f41b2629c8c424a3a0c3b33d66a508a
Danny Toeun Kim <34687639+kimdanny@users.noreply.github.com>
2019-04-02 14:35:54 +0100

    Update AbstractBag.java

--- src/main/java/uk/ac/ucl/bag/AbstractBag.java
+++ src/main/java/uk/ac/ucl/bag/AbstractBag.java
@@ -7,7 +7,6 @@ package uk.ac.ucl.bag;
  * New bag objects are created using a BagFactory, which can be configured in the application
  * setup to select which bag implementation is to be used.
  */
-import java.util.Iterator;
 
 public abstract class AbstractBag<T extends Comparable> implements Bag<T>
 {
@@ -35,6 +34,55 @@ public abstract class AbstractBag<T extends Comparable> implements Bag<T>
       if (!result.contains(value)) result.add(value);
     }
     return result;
+  }
+
+  @Override
+  public String toString(){
+      boolean first = true;
+      String str ="";
+      str += "[";
+      for (T value : this){
+          if (!first){str += ", ";}
+          first = false;
+
+          T key = value;
+          int occurrence = this.countOf(value);
+          str += key;
+          str += ":";
+          str += occurrence;
+      }
+
+      str += "]";
+
+      return str;
+  }
+
+
+  //It makes sure that the occurrence count of all values stored in a Bag is set to one
+  public void removeAllCopies(){
+      for (T value : this){
+          int occurrences = this.countOf(value);
+          while (occurrences > 1){
+              this.remove(value);
+              --occurrences;
+          }
+      }
+
+  }
+
+
+    // returns a new Bag containing all values and occurrences that occur in the 'this' bag but not the argument bag
+  public Bag<T> subtract(Bag<T> bag){
+
+      for (T value : bag){
+          if (bag.contains(value) && this.contains(value)){
+              int occurrences = bag.countOf(value);
+              for (int i = 0; i < occurrences; i++){
+                  this.remove(value);
+              }
+          }
+      }
+      return this;
   }
 
 }
